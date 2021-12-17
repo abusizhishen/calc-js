85e0641353c28b4c99b8e845c72cecb5dd50f742
abusizhishen <abusizhishen@gmail.com>
2021-12-17 23:38:31 +0800

    计算，优先级有问题

--- parser.js
+++ parser.js
@@ -1,121 +1,149 @@
-const Numeric = {type:1,Text:"number"}
-const  StringType={type:2,Text:"string"}
-const  AddSymbol={type:3,Text:"+"}
-const  SubSymbol={type:4,Text:"-"}
-const  MulSymbol={type:5,Text:"*"}
-const  DivSymbol={type:6,Text:"/"}
-const  LeftParenthesisSymbol={type:7,Text:"("}
-const  RightParenthesisSymbol = {type:8,Text:")"}
-
-
-
+const priorityAddSub = 0
+const priorityMulDiv = 1
 class Token{
-    constructor(type,text){
-        this.type = type
-        this.text = text
-    }
+  constructor(type,text,priority){
+    this.type = type
+    this.text = text
+    this.priority = priority
+  }
+
+  value(){
+    return Number(this.text)
+  }
 }
 
+class AstNode {
+  constructor(left,symbol,right) {
+    this.left = left
+    this.symbol = symbol
+    this.right = right
+  }
+
+  value(){
+    switch (this.symbol.type) {
+      case AddSymbol.type:
+        return add(this.left.value(),this.right.value())
+      case SubSymbol.type:
+        return sub(this.left.value(),this.right.value())
+      case MulSymbol.type:
+        return mul(this.left.value(),this.right.value())
+      case DivSymbol.type:
+        return div(this.left.value(),this.right.value())
+      default:
+        throw ("unknown symbol: "+this.symbol.type)
+    }
+  }
+}
+
+const  Numeric = new Token(1,"number",)
+const  StringType=new Token(2,"string")
+const  AddSymbol=new Token(3,"+",priorityAddSub)
+const  SubSymbol=new Token(4,"-",priorityAddSub)
+const  MulSymbol=new Token(5,"*",priorityMulDiv)
+const  DivSymbol=new Token(6,"/",priorityMulDiv)
+const  LeftParenthesisSymbol=new Token(7,"(")
+const  RightParenthesisSymbol = new Token(8,")")
+
 class Lexer {
-    constructor(code) {
-        this.code = code;
-        this.position = 0
-        this.readPosition = 0;
-        this.length = code.length
-        this.ch = 0
+  constructor(code) {
+    this.code = code;
+    this.position = 0
+    this.readPosition = 0;
+    this.length = code.length
+    this.ch = 0
 
-        console.log(this.code)
+    console.log(this.code)
+  }
+
+  parser(){
+    var tokens = []
+    var token = this.nextToken()
+    while(token){
+      tokens.push(token)
+      token = this.nextToken()
     }
 
-    parser(){
-        var tokens = []
-        var token = this.nextToken()
-        while(token){
-            tokens.push(token)
-            token = this.nextToken()
-        }
+    return tokens
+  }
 
-        return tokens
+  readNumeric(){
+    var ch = this.readNextCh()
+    var position = this.position
+    while(ch !== 0){
+      if (!this.isNumber(ch)) {
+        break
+      }
+      this.position = this.readPosition
+      this.readPosition++
+      ch = this.readNextCh()
     }
 
-    readNumeric(){
-        var ch = this.readNextCh()
-        var position = this.position
-        while(ch !== 0){
-            if (!this.isNumber(ch)) {
-                break
-            }
-            this.position = this.readPosition
-            this.readPosition++
-            ch = this.readNextCh()
-        }
+    return new Token(Numeric.type, this.code.substring(position,this.readPosition))
+  }
 
-        return new Token(Numeric.type, this.code.substring(position,this.readPosition))
+  readCh(){
+    if (this.readPosition < this.length){
+      this.ch = this.code.charCodeAt(this.readPosition)
+    }else{
+      this.ch = 0
+    }
+    this.position = this.readPosition
+    this.readPosition++
+  }
+
+  readNextCh(){
+    if (this.readPosition < this.length){
+      return this.code.charCodeAt(this.readPosition)
+    }else{
+      return 0
+    }
+  }
+
+  isNumber(ch){
+    return ch >=48 && ch <= 57
+  }
+
+  nextToken(){
+    this.skipSpace()
+    this.readCh()
+
+    if (this.ch === 0){
+      return null
+    }
+    if (this.isNumber(this.ch)) {
+      return this.readNumeric()
     }
 
-    readCh(){
-        if (this.readPosition < this.length){
-            this.ch = this.code.charCodeAt(this.readPosition)
-        }else{
-            this.ch = 0
-        }
-        this.position = this.readPosition
-        this.readPosition++
+    switch (String.fromCharCode(this.ch)) {
+      case AddSymbol.text:
+        return AddSymbol
+      case SubSymbol.text:
+        return SubSymbol
+      case MulSymbol.text:
+        return MulSymbol
+      case DivSymbol.text:
+        return DivSymbol
+      case LeftParenthesisSymbol.text:
+        return LeftParenthesisSymbol
+      case RightParenthesisSymbol.text:
+        return RightParenthesisSymbol
+      default:
+        throw "invalid char: "
     }
 
-    readNextCh(){
-        if (this.readPosition < this.length){
-            return this.code.charCodeAt(this.readPosition)
-        }else{
-            return 0
-        }
+  }
+
+  skipSpace(){
+    while(true){
+      switch (String.fromCharCode(this.readNextCh())){
+        case " ":
+          this.readCh()
+          break
+        default:
+          return
+      }
     }
-
-    isNumber(ch){
-        return ch >=48 && ch <= 57
-    }
-
-    nextToken(){
-        this.skipSpace()
-        this.readCh()
-
-        if (this.ch === 0){
-          return null
-        }
-        if (this.isNumber(this.ch)) {
-            return this.readNumeric()
-        }
-
-        switch (String.fromCharCode(this.ch)) {
-          case AddSymbol.Text:
-            return AddSymbol
-          case SubSymbol.Text:
-            return SubSymbol
-          case MulSymbol.Text:
-            return MulSymbol
-          case DivSymbol.Text:
-            return DivSymbol
-          case LeftParenthesisSymbol:
-            return LeftParenthesisSymbol
-          case RightParenthesisSymbol:
-            return RightParenthesisSymbol
-          default:
-            throw "invalid char: "
-        }
-
-    }
-
-    skipSpace(){
-        while(true){
-            switch (String.fromCharCode(this.readNextCh())){
-                case " ":
-                    this.readCh()
-                    break
-                default:
-                    return
-            }
-        }
-    }
+  }
 }
 
 class Parser{
@@ -141,9 +169,6 @@ class Parser{
     return null
   }
 
-  readCalculateSymbol(){
-    return this.readToken()
-  }
   readMulExpression(){}
   parser(){
     var expression = this.nextExpression()
@@ -163,21 +188,45 @@ class Parser{
     switch (token.type){
       case Numeric.type:
         var left = token
-        var symbol = this.readCalculateSymbol()
+        var symbol = this.readToken()
         var nextToken = this.readNextToken()
         if (nextToken.type === Numeric.type){
           this.readToken()
-          return {
-            left:left,
-            symbol:symbol,
-            right:nextToken
+          var node = new AstNode(
+            left,
+            symbol,
+            nextToken
+          )
+
+          var nextSymbol = this.readNextToken()
+          if (nextSymbol == null){
+            return node
           }
+
+          if (symbol.priority === nextSymbol.priority) {
+              return node
+          }
+
+          if (symbol.priority < nextSymbol.priority){
+            return new AstNode(
+              left,
+              symbol,
+              new AstNode(
+                nextToken,
+                nextSymbol,
+                this.nextExpression()
+              ))
+
+          }
+
+          return new AstNode(
+            left,
+            symbol,
+            nextToken
+          )
         }else{
-          return {
-            left:left,
-            symbol:symbol,
-            right:this.nextExpression()
-          }
+
+          throw ("invalid token: "+ nextToken.type)
         }
       case AddSymbol.type:
       case SubSymbol.type:
@@ -187,25 +236,43 @@ class Parser{
         var nextToken = this.readNextToken()
         if (nextToken.type === Numeric.type){
           this.readToken()
-            right = nextToken
+          right = nextToken
         }else{
           var right = this.nextExpression()
         }
 
-        return {
-          left:left,
-          symbol:token.type,
-          right:right
-        }
+        return new AstNode(
+          left,
+          token,
+          right
+        )
       default:
         throw "unknown type"+token.text
     }
   }
 }
 
+function add(a,b) {
+  return a+b
+}
 
-let lexer = new Lexer("123*2+4*5");
+function sub(a,b) {
+  return a-b
+}
+
+function mul(a,b) {
+  return a*b
+}
+
+function div(a,b) {
+  return a/b
+}
+
+
+let lexer = new Lexer("123*2+4*0");
 tokens = lexer.parser();
 parser = new Parser(tokens)
-var expressions = parser.parser()
-console.log(JSON.stringify(expressions))
+var ast = parser.parser()[0]
+console.log(JSON.stringify([ast]))
+console.log(ast.value())
+
